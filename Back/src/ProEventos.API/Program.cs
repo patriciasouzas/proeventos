1f95cd9f6cdc0a361d506d592f2821a41f87456b
patriciasouzas <patriciasouzaa89@gmail.com>
2023-09-13 21:32:55 -0300

    alterações

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -1,11 +1,17 @@
+
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(
+    context => context.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+    );
 
 var app = builder.Build();
 
