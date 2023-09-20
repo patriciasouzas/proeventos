8bdf983c98d8a6e03c92e71f5660ef3b4979b5bf
patriciasouzas <patriciasouzaa89@gmail.com>
2023-09-19 21:59:21 -0300

    modificações

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
   ],
   imports: [
     BrowserModule
