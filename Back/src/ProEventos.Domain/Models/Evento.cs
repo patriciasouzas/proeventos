1e4c80489e58ea128bd11fb5f3548845ff7b0055
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-08 22:46:31 -0300

    alterações nas camadas do projeto

---
+++ Back/src/ProEventos.Domain/Models/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProEventos.Domain.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public DateTime? DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+		public IEnumerable<Lote> Lote { get; set; }
+		public IEnumerable<RedeSocial> RedesSociais { get; set; }
+	}
+}
