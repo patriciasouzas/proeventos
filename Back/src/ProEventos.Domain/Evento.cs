47de232ff71f11704b579b8bd802c966adb48294
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-15 22:14:18 -0300

    ajustes para reestruturação no projeto

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -4,7 +4,7 @@ namespace ProEventos.Domain
 {
     public class Evento
     {
-        public int EventoId { get; set; }
+        public int Id { get; set; }
         public string Local { get; set; }
         public DateTime? DataEvento { get; set; }
         public string Tema { get; set; }
@@ -12,7 +12,7 @@ namespace ProEventos.Domain
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<Lote> Lote { get; set; }
+        public IEnumerable<Lote> Lotes { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
         public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
     }
---
+++ Back/src/ProEventos.Persistence/ProEventosContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options)
+        : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestranteEventos { get; set; }
+        public DbSet<RedeSocial> RedeSociais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(Persistence => new
+                {
+                    PE.EventoId,
+                    PE.PalestranteId
+                })
+        }
+    }
+}
+i
